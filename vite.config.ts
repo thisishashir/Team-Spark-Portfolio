361bf49857627cd1e9cfdbb233c5dbbe763c2360
Hashir Qureshi <qrekhan786@gmail.com>
2025-12-18 13:02:13 +0530

    Remove base path for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,10 @@ import { fileURLToPath } from 'url'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// 👇 FIX for __dirname in ESM
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  base: '/Team-Spark-Portfolio/', // GitHub Pages fix
   plugins: [
     react(),
     tailwindcss(),
